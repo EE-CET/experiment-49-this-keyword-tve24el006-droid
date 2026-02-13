4a8a5a4151f0f7c2f964a1732d22401e29f0dcb3
ALAN SHAJAN <tve24el006cet.ac.in>
2026-02-13 09:48:52 +0530

    fortyninthcommit

--- src/ThisKeywordDemo.java
+++ src/ThisKeywordDemo.java
@@ -3,19 +3,32 @@ import java.util.Scanner;
 class ThisDemo {
     int value;
 
-    // TODO: Define setValue(int value)
-    // Use 'this.value = value' to resolve naming conflict
-    
-    // TODO: Define display() to print "Value: " + value
+    // Using this to resolve naming conflict
+    void setValue(int value) {
+        this.value = value;
+    }
+
+    void display() {
+        System.out.println("Value: " + value);
+    }
 }
 
 public class ThisKeywordDemo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // TODO: Read the integer input
-        
-        // TODO: Create ThisDemo object
-        // TODO: Call setValue() with the input
-        // TODO: Call display()
+
+        // Read input
+        int input = sc.nextInt();
+
+        // Create object
+        ThisDemo obj = new ThisDemo();
+
+        // Set value
+        obj.setValue(input);
+
+        // Display value
+        obj.display();
+
+        sc.close();
     }
 }
